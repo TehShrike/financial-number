22303748c921f489bd59abd7e082924016c32ef1
Josh Duff <me@JoshDuff.com>
2019-02-13 09:52:13 -0600

    Modern JS
    It looks nicer, and it saves a dozen-odd lines

--- index.js
+++ index.js
@@ -1,53 +1,49 @@
-var math = require('financial-arithmetic-functions')
+const math = require(`financial-arithmetic-functions`)
 
-var allZeroesRegex = /^0(\.0+)?$/
-var someNonZeroDigit = /[1-9]/
+const allZeroesRegex = /^0(\.0+)?$/
+const someNonZeroDigit = /[1-9]/
 
-var makeNumberObject = makeArgumentString(function makeNumberObject(str) {
-	var self = {
-		plus: makeArgumentString(function plus(otherSide) {
-			return makeNumberObject(math.add(str, otherSide))
-		}),
-		minus: makeArgumentString(function minus(otherSide) {
-			return makeNumberObject(math.subtract(str, otherSide))
-		}),
-		times: makeArgumentString(function times(otherSide) {
-			return makeNumberObject(math.multiply(str, otherSide))
-		}),
-		getPrecision: function getPrecision() {
+const makeArgumentString = fn => input => fn(typeof input === `string` ? input : input.toString())
+
+const makeNumberObject = makeArgumentString(str => {
+	const self = {
+		plus: makeArgumentString(otherSide => makeNumberObject(math.add(str, otherSide))),
+		minus: makeArgumentString(otherSide => makeNumberObject(math.subtract(str, otherSide))),
+		times: makeArgumentString(otherSide => makeNumberObject(math.multiply(str, otherSide))),
+		getPrecision() {
 			return math.getPrecision(str)
 		},
-		toString: function toString(precision, roundingStrategy) {
-			if (typeof precision === 'number') {
+		toString(precision, roundingStrategy) {
+			if (typeof precision === `number`) {
 				return self.changePrecision(precision, roundingStrategy).toString()
 			}
 			return str
 		},
-		changePrecision: function changePrecision(precision, roundingStrategy) {
+		changePrecision(precision, roundingStrategy) {
 			roundingStrategy = roundingStrategy || module.exports.trim
 
 			return makeNumberObject(trimNegationFromZero(roundingStrategy(makeNumberObject(str), precision)))
 		},
-		isNegative: function() {
-			return self.toString()[0] === '-'
+		isNegative() {
+			return self.toString()[0] === `-`
 		},
-		equal: function equal(otherSide) {
-			var difference = makeNumberObject(otherSide).minus(self).toString()
+		equal(otherSide) {
+			const difference = makeNumberObject(otherSide).minus(self).toString()
 			return allZeroesRegex.test(difference)
 		},
-		gt: function greaterThan(otherSide) {
-			var difference = self.minus(otherSide)
+		gt(otherSide) {
+			const difference = self.minus(otherSide)
 			return !difference.isNegative() && someNonZeroDigit.test(difference.toString()) && !self.equal(otherSide)
 		},
-		gte: function greaterThanOrEqualTo(otherSide) {
+		gte(otherSide) {
 			return self.gt(otherSide) || self.equal(otherSide)
 		},
-		lt: function lessThan(otherSide) {
+		lt(otherSide) {
 			return !self.gte(otherSide)
 		},
-		lte: function lessThanOrEqualTo(otherSide) {
+		lte(otherSide) {
 			return !self.gt(otherSide)
-		}
+		},
 	}
 
 	return self
@@ -55,58 +51,47 @@ var makeNumberObject = makeArgumentString(function makeNumberObject(str) {
 module.exports = makeNumberObject
 module.exports.trim = adjustPrecisionByTrimming
 module.exports.round = function adjustPrecisionByRounding(number, targetPrecision) {
-	var currentPrecision = number.getPrecision()
-	var precisionIsDropping = targetPrecision < currentPrecision
+	const currentPrecision = number.getPrecision()
+	const precisionIsDropping = targetPrecision < currentPrecision
 
 	if (precisionIsDropping) {
-		var changeForRounding = makeNumberObject('0.' + zeroes(targetPrecision) + '5')
-		var operation = number.isNegative() ? number.minus : number.plus
+		const changeForRounding = makeNumberObject(`0.` + zeroes(targetPrecision) + `5`)
+		const operation = number.isNegative() ? number.minus : number.plus
 		number = operation(changeForRounding)
 	}
 
 	return adjustPrecisionByTrimming(number, targetPrecision)
 }
 
-// Trimming!
 function adjustPrecisionByTrimming(number, targetPrecision) {
-	var currentPrecision = number.getPrecision()
-	var str = number.toString()
+	const currentPrecision = number.getPrecision()
+	const str = number.toString()
 	if (currentPrecision === targetPrecision) {
 		return str
 	} else if (targetPrecision === 0) {
-		return str.split('.')[0]
+		return str.split(`.`)[0]
 	} else if (currentPrecision === 0) {
-		return str + '.' + zeroes(targetPrecision)
+		return str + `.` + zeroes(targetPrecision)
 	} else if (targetPrecision < currentPrecision) {
-		var charactersToRemove = currentPrecision - targetPrecision
+		const charactersToRemove = currentPrecision - targetPrecision
 		return str.substring(0, str.length - charactersToRemove)
 	} else if (currentPrecision < targetPrecision) {
-		var zeroesToAdd = targetPrecision - currentPrecision
+		const zeroesToAdd = targetPrecision - currentPrecision
 		return str + zeroes(zeroesToAdd)
 	}
 }
 
 function zeroes(times) {
-	var output = ''
-	for (var i = 0; i < times; ++i) {
-		output += '0'
+	let output = ``
+	for (let i = 0; i < times; ++i) {
+		output += `0`
 	}
 	return output
 }
 
-function makeArgumentString(fn) {
-	return function(input) {
-		return fn(toString(input))
-	}
-}
-
-function toString(input) {
-	return typeof input === 'string' ? input : input.toString()
-}
-
 function trimNegationFromZero(str) {
-	if (str.length >= 2 && str[0] === '-') {
-		var restOfNumberAfterDash = str.substring(1)
+	if (str.length >= 2 && str[0] === `-`) {
+		const restOfNumberAfterDash = str.substring(1)
 		return allZeroesRegex.test(restOfNumberAfterDash) ? restOfNumberAfterDash : str
 	}
 	return str
